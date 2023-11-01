d7ed10d5df293bd379959947286880f308fa8003
BB <bb@localhost.localdomain>
2023-11-01 21:02:52 +0100

    1

--- Homework_01.py
+++ Homework_01.py
@@ -13,9 +13,49 @@ while print("Whiskey"):
 
 s = 0
 
-while s < 5:
+while s < len(SHOTS):
     print(SHOTS[s])
     s = s + 1
 
 
-PEOPLE = 
+PEOPLE = [
+    {
+        "firstName": "James",
+        "lastName": "Bond",
+        "favorites": ["Vodka", "Martini"]
+    },
+    {
+        "firstName": "Hans",
+        "lastName": "Rehgenkurt",
+        "favorites": ["Martini", "Martini"]
+    },
+    {
+        "firstName": "B",
+        "lastName": "BÉ",
+        "favorites": ["Pálinka"]
+    },
+]
+
+j = 0
+
+while j < len(PEOPLE):
+    firstName = PEOPLE[j]["firstName"]
+    lastName = PEOPLE[j]["lastName"]
+    favorites = PEOPLE[j]["favorites"]
+    print(f"{firstName} {lastName}")
+
+    van = False
+    k = 0
+    while k < len(favorites):
+        l = 0
+        while l < len(SHOTS):
+            if favorites[k] == SHOTS[l]:
+                van = True
+            l = l + 1
+        k = k + 1
+    j = j + 1
+
+    if van:
+        print("A szokásosat?")
+    else:
+        print("A készlet kifogyott!")
